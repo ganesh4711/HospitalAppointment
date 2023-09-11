8fa8fe5febcfd3f4d78a4fc41928c33c6226bd20
ganesh4711 <ssganesh471@gmail.com>
2023-09-11 10:43:59 +0530

    added staff services

--- src/main/java/com/main/controllers/ReceptionStaffController.java
+++ src/main/java/com/main/controllers/ReceptionStaffController.java
@@ -1,22 +1,125 @@
 package com.main.controllers;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
+import com.main.ApiResponse;
+import com.main.RequestDto.PatientDto;
+import com.main.RequestDto.ReceptionStaffDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.main.entites.ReceptionStaff;
 import com.main.repos.ReceptionStaffRepository;
 import com.main.service.ReceptionStaffService;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("/staff")
 public class ReceptionStaffController {
 
 	@Autowired
 	private ReceptionStaffService receptionService;
-	@GetMapping("/get/receptionstaff")
-	public List<ReceptionStaff> getStaff(){
-		return receptionService.getStaff();
+
+	/**
+	 *
+	 * @return staff member info
+	 */
+	@GetMapping("/{id}")
+	public ReceptionStaffDto getStaffInfoById(@PathVariable int id){
+		return receptionService.findReceptionInfoWithId(id);
 	}
+
+
+	/**
+	 *
+	 * @return staff members info
+	 */
+	@GetMapping("all")
+	public ApiResponse<List<ReceptionStaffDto>> getStaffInfo(){
+		Map<String, Object> data = new HashMap<>();
+		data.put("Staff", receptionService.findAllStaffInfo());
+		return new ApiResponse<>(HttpStatus.OK,"SUCCESS",data);
+	}
+	/**
+	 * @param offset   page start from 1
+	 * @param pagesize of the page
+	 * @return page of patients
+	 */
+	@GetMapping("/{offset}/{pagesize}")
+	public ApiResponse<ReceptionStaffDto> retriveAllWithPagination(@PathVariable int offset, @PathVariable int pagesize) {
+		if (offset < 1 || pagesize < 1) {
+			throw new IllegalArgumentException();
+		}
+		Page<ReceptionStaffDto> allStaffWithPagination = receptionService.findAllStaffWithPagination((offset-1), pagesize);
+		if (offset <= allStaffWithPagination.getTotalPages()) {
+			Map<String, Object> data = new HashMap<>();
+			data.put("Staff", allStaffWithPagination.getContent());
+			Map<String, Object> meta = Map.of("pageNo", offset,
+					"pageSize", allStaffWithPagination.getSize(),
+					"pageCount", allStaffWithPagination.getNumberOfElements(),
+					"recordCount", allStaffWithPagination.getTotalElements(),
+					"noOfPages", allStaffWithPagination.getTotalPages());
+
+			return new ApiResponse<>(HttpStatus.OK, "SUCCESS", data, meta);
+
+		} else
+			return new ApiResponse<>(HttpStatus.NO_CONTENT, "Empty page");
+	}
+
+	/**
+	 * @param offset   page
+	 * @param pageSize of the page
+	 * @param field    to be sorted
+	 * @return page of staff
+	 */
+	@GetMapping("/{offset}/{pageSize}/{field}")
+	public ApiResponse<ReceptionStaffDto> retriveAllWithPaginationSorting(@PathVariable int offset, @PathVariable int pageSize, @PathVariable String field) {
+		if (offset < 1 || pageSize < 1) {
+			throw new IllegalArgumentException();
+		}
+		Page<ReceptionStaffDto> allPatientsWithPagination = receptionService.findAllStaffWithPaginationSorting((offset-1), pageSize,field);
+		if (offset <= allPatientsWithPagination.getTotalPages()) {
+			Map<String, Object> data = new HashMap<>();
+			data.put("user", allPatientsWithPagination.getContent());
+			Map<String, Object> meta = Map.of("pageNo", offset,
+					"pageSize", allPatientsWithPagination.getSize(),
+					"pageCount", allPatientsWithPagination.getNumberOfElements(),
+					"recordCount", allPatientsWithPagination.getTotalElements(),
+					"noOfPages", allPatientsWithPagination.getTotalPages());
+
+			return new ApiResponse<>(HttpStatus.OK, "SUCCESS", data, meta);
+
+		} else
+			return new ApiResponse<>(HttpStatus.NO_CONTENT, "Empty page");
+	}
+	/**
+	 *
+	 * @param receptionStaffDto object
+	 * @return staff info
+	 */
+	@PostMapping("/signup")
+	public ReceptionStaffDto addReceptionStaff(@RequestBody @Valid ReceptionStaffDto receptionStaffDto) {
+		return receptionService.addStaff(receptionStaffDto);
+	}
+
+	/**
+	 *
+	 * @param id of staff
+	 * @return deactive status
+	 */
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse<Void>> deletePatientWithId(@PathVariable int id){
+		if (receptionService.deleteStaffWithId(id)){
+			return new ResponseEntity<>(new ApiResponse<>(HttpStatus.NO_CONTENT,"Patient Deleted"),HttpStatus.NO_CONTENT);
+		}
+		else  throw new NoSuchElementException();
+	}
+
 }
--- src/main/java/com/main/service/ReceptionStaffService.java
+++ src/main/java/com/main/service/ReceptionStaffService.java
@@ -1,11 +1,23 @@
 package com.main.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.main.RequestDto.PatientDto;
+import com.main.entites.Patient;
+import com.main.entites.User;
+import com.main.globalExcp.BussinessException;
+import com.main.repos.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.main.RequestDto.ReceptionStaffDto;
@@ -15,18 +27,105 @@ import com.main.repos.ReceptionStaffRepository;
 @Service
 public class ReceptionStaffService {
 
-	@Autowired
-	private ReceptionStaffRepository receptionRepo;
-	
-	@Autowired
-	private ModelMapper modelmapper;
-	
-	public ReceptionStaffDto convertEntityToDto(ReceptionStaff staff ) {
-		modelmapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-		return modelmapper.map(staff, ReceptionStaffDto.class);
-	}
-	
-	public List<ReceptionStaff> getStaff(){
-		return receptionRepo.findAll();
-	}
+    @Autowired
+    private ReceptionStaffRepository receptionRepo;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelmapper;
+
+    public ReceptionStaffDto convertEntityToDto(ReceptionStaff staff) {
+        modelmapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelmapper.map(staff, ReceptionStaffDto.class);
+    }
+
+    public ReceptionStaff converDtoToEntity(ReceptionStaffDto receptionStaffDto) {
+        return modelmapper.map(receptionStaffDto, ReceptionStaff.class);
+    }
+
+    /**
+     * @param id staff id
+     * @return staff details
+     */
+    public ReceptionStaffDto findReceptionInfoWithId(int id) {
+        Optional<ReceptionStaff> byId = receptionRepo.findById(id);
+        if (byId.isPresent()) {
+            ReceptionStaff receptionStaff = byId.get();
+            return convertEntityToDto(receptionStaff);
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     *
+     */
+    public List<ReceptionStaffDto> findAllStaffInfo() {
+        return receptionRepo.findAll().stream()
+                .map(this::convertEntityToDto)
+                .toList();
+    }
+
+    /**
+     * @param offSet   page
+     * @param pagesize of the page
+     * @return page
+     */
+    public Page<ReceptionStaffDto> findAllStaffWithPagination(int offSet, int pagesize) {
+        Page<ReceptionStaff> receptionStaffPage = receptionRepo.findAll(PageRequest.of(offSet, pagesize));
+        List<ReceptionStaffDto> userDtoList = receptionStaffPage.getContent().stream()
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(userDtoList, receptionStaffPage.getPageable(), receptionStaffPage.getTotalElements());
+    }
+
+    /**
+     * @param offSet   page
+     * @param pagesize of the page
+     * @param field    to be sorted
+     * @return page
+     */
+    public Page<ReceptionStaffDto> findAllStaffWithPaginationSorting(int offSet, int pagesize, String field) {
+
+        Page<ReceptionStaff> staffPage = receptionRepo.findAll(PageRequest.of(offSet, pagesize, Sort.by(field)));
+        List<ReceptionStaffDto> receptionStaffDtosList = staffPage.getContent().stream()
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(receptionStaffDtosList, staffPage.getPageable(), staffPage.getTotalElements());
+    }
+
+    public ReceptionStaffDto addStaff(ReceptionStaffDto receptionStaffDto) {
+        User user = userRepository.findById(25).get();
+        if (user.getStatus()) {
+            if (receptionStaffDto != null) {
+                receptionStaffDto.setStaffName(user.getName());
+                receptionStaffDto.setStatus(true);
+                ReceptionStaff staff = converDtoToEntity(receptionStaffDto);
+                return convertEntityToDto(receptionRepo.save(staff));
+
+            } else throw new BussinessException("Patient Must not Be Null");
+        } else throw new BussinessException("Deactivated User Unable to Process...");
+    }
+
+    /**
+     * @param id patient to be deactivated
+     * @return execution status
+     */
+    public boolean deleteStaffWithId(int id) {
+        Optional<ReceptionStaff> optional = receptionRepo.findById(id);
+        if (optional.isPresent()) {
+            ReceptionStaff receptionStaff = optional.get();
+            if (receptionStaff.getStatus()) {
+                receptionStaff.setStatus(false);
+                return true;
+            }
+        } else
+            return false;
+
+        return false;
+    }
 }
+
+
