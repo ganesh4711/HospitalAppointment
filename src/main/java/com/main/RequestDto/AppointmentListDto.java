a2b6f4b561eb09658fc4134106b75a61dc29b0b9
ganesh4711 <ssganesh471@gmail.com>
2023-09-19 14:24:50 +0530

    added post service

--- src/main/java/com/main/RequestDto/AppointmentListDto.java
+++ src/main/java/com/main/RequestDto/AppointmentListDto.java
@@ -1,8 +1,13 @@
 package com.main.RequestDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 
@@ -12,11 +17,20 @@ public class AppointmentListDto
 {
 
     private Long id;
-    private Long doctorId;
-    private Long patientId;
+    @NotBlank(message = "doctor id cannot be blank")
+    private Integer doctorId;
+    @NotBlank(message = "patient id cannot be blank")
+    private Integer  patientId;
+    @NotNull(message = "type not be null")
     private String type;
+    @NotBlank(message = "staff id cannot be blank")
     private Long receptionStaffId;
-    private Date dateOfAppointment;
+
+    @NotNull(message = "Date not be null")
+    @DateTimeFormat(pattern =  "MM/dd/yyyy")
+    private LocalDate dateOfAppointment;
+    @NotNull(message = "Date not be null")
+    @DateTimeFormat(pattern =  "HH:mm")
     private LocalTime timeOfAppointment;
     private Boolean status;
 
--- src/main/java/com/main/service/AppointmentListService.java
+++ src/main/java/com/main/service/AppointmentListService.java
@@ -2,15 +2,27 @@ package com.main.service;
 
 import com.main.RequestDto.AppointmentListDto;
 import com.main.entites.AppointmentList;
+import com.main.entites.Doctor;
+import com.main.entites.Patient;
+import com.main.globalExcp.BussinessException;
 import com.main.repos.AppointmentListRepo;
+import com.main.repos.DoctorRepository;
+import com.main.repos.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 @AllArgsConstructor
 public class AppointmentListService {
     private final ModelMapper modelMapper;
     private final AppointmentListRepo appointmentListRepo;
+    private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
     public AppointmentListDto convertEntityToDto(AppointmentList appointmentList){
         return modelMapper.map(appointmentList,AppointmentListDto.class);
     }
@@ -29,5 +41,33 @@ public class AppointmentListService {
         return allByDoctorDoctorId.stream()
                 .map(this::convertEntityToDto)
                 .toList();
+    }
+    public AppointmentListDto appointmentRequest(AppointmentListDto appointmentListDto) {
+        AppointmentList appointmentList=null;
+        Optional<Patient> byId = patientRepository.findById(appointmentListDto.getPatientId());
+        if (byId.isPresent()) {
+            Patient patient = byId.get();
+            if (patient.getStatus().equals(true)) {
+                Optional<Doctor> optionalDoctor = doctorRepository.findById(appointmentListDto.getDoctorId());
+                if (optionalDoctor.isPresent()) {
+                    Doctor doctor = optionalDoctor.get();
+                    if (doctor.getStatus().equals(true) && doctor.getType().equals(appointmentListDto.getType())) {
+                        if (!appointmentListDto.getDateOfAppointment().isBefore(LocalDate.now())){
+                            LocalTime startTime = LocalTime.of(10, 0);
+                            LocalTime endTime = LocalTime.of(18, 0);
+                            if (appointmentListDto.getTimeOfAppointment().isAfter(startTime) && appointmentListDto.getTimeOfAppointment().isBefore(endTime))
+                            { appointmentListDto.setStatus(null);
+                                appointmentList = appointmentListRepo.save(convertDtoToEntity(appointmentListDto));
+                            }
+                            else throw new BussinessException("Appointment timings with in 10:00 - 6:00 PM")
+                        }
+                        else throw new BussinessException("Invalid Date ");
+                    }
+                    else throw new BussinessException("Invalid Doctor ");
+                }else throw new NoSuchElementException();
+            }
+            else throw new BussinessException("Invalid Patient");
+        }
+        return convertEntityToDto(appointmentList);
     }
 }
